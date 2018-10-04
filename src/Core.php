b9a1128a7792f5aac75a1c998511e4fa83edf50f
Joel <jdasilva@corp.free.fr>
2018-10-04 18:55:27 +0200

    Get data on new instance
    Or not, if no id provided

--- src/Core.php
+++ src/Core.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace ild78;
 
+use DateTime;
+use GuzzleHttp;
+use ild78\Exceptions;
+
 /**
  * Manage common code between API object
  */
@@ -13,6 +17,82 @@ abstract class Core
 
     /** @var string */
     protected $id;
+
+    /** @var DateTime */
+    protected $created;
+
+    /**
+     * Create or get an API object
+     *
+     * @param string|null $id Object id
+     * @return self
+     * @throws ild78\Exceptions\TooManyRedirectsException on too many redirection case (HTTP 310)
+     * @throws ild78\Exceptions\NotAuthorizedException on credential problem (HTTP 401)
+     * @throws ild78\Exceptions\NotFoundException if an `id` is provided but it seems unknonw (HTTP 404)
+     * @throws ild78\Exceptions\ClientException on HTTP 4** errors
+     * @throws ild78\Exceptions\ServerException on HTTP 5** errors
+     * @throws ild78\Exceptions\Exception on every over exception send by GuzzleHttp
+     */
+    public function __construct(string $id = null)
+    {
+        if ($id) {
+            $api = Api::getInstance();
+            $client = $api->getHttpClient();
+
+            $options = [
+                'headers' => [
+                    'Authorization' => 'Basic ' . $api->getKey(),
+                ],
+            ];
+
+            try {
+                $response = $client->request('GET', $this->getEndpoint() . '/' . $id);
+            } catch (GuzzleHttp\Exception\ServerException $exception) { // HTTP 5**
+                $message = 'Servor error, please leave a minute to repair it and try again';
+                throw new Exceptions\ServerException($message, 0, $exception);
+            } catch (GuzzleHttp\Exception\TooManyRedirectsException $exception) {
+                throw new Exceptions\TooManyRedirectsException('Too many redirection', 0, $exception);
+            } catch (GuzzleHttp\Exception\ClientException $exception) { // HTTP 4**
+                $response = $exception->getResponse();
+                $class = Exceptions\ClientException::class;
+                $message = vsprintf('%d - %s', [
+                    $response->getStatusCode(),
+                    $response->getReasonPhrase(),
+                ]);
+
+                switch ($response->getStatusCode()) {
+                    case 400:
+                        $class = Exceptions\BadRequestException::class;
+                        break;
+
+                    case 401:
+                        $body = json_decode((string) $response->getBody());
+                        $class = Exceptions\NotAuthorizedException::class;
+                        $message = $body->error->message;
+                        break;
+
+                    case 404:
+                        $class = Exceptions\NotFoundException::class;
+                        $message = sprintf('%s "%s" unknonw', get_class($this), $id);
+                        break;
+                }
+
+                throw new $class($message, 0, $exception);
+            } catch (Exception $exception) {
+                throw new Exceptions\Exception('Unknown error, may be a network error', 0, $exception);
+            }
+
+            $body = json_decode((string) $response->getBody(), true);
+
+            foreach ($body as $key => $value) {
+                if ($key === 'created') {
+                    $this->created = new DateTime('@' . $value);
+                } elseif (property_exists($this, $key)) {
+                    $this->$key = $value;
+                }
+            }
+        }
+    }
 
     /**
      * Return API endpoint
@@ -32,5 +112,15 @@ abstract class Core
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Return creation date
+     *
+     * @return DateTime|null
+     */
+    public function getCreationDate()
+    {
+        return $this->created;
     }
 }
--- tests/unit/Core.php
+++ tests/unit/Core.php
@@ -3,10 +3,146 @@
 namespace ild78\tests\unit;
 
 use atoum;
+use GuzzleHttp;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use ild78\Api;
 use ild78\Core as testedClass;
+use ild78\Exceptions;
 
 class Core extends atoum
 {
+    public function test__construct()
+    {
+        $this
+            ->given($id = uniqid())
+            ->and($timestamp = rand())
+            ->and($mock = new MockHandler([
+                new Response(200, [], '{"id":"' . $id . '","created":' . $timestamp . '}'),
+                new Response(401, [], file_get_contents(__DIR__ . '/fixtures/auth/not-authorized.json')),
+            ]))
+            ->and($handler = HandlerStack::create($mock))
+            ->and($client = new Client(['handler' => $handler]))
+            ->and($api = new Api(uniqid()))
+            ->and($api->setHttpClient($client))
+
+            ->assert('Without id')
+                ->given($this->newTestedInstance())
+                    ->variable($this->testedInstance->getId())
+                        ->isNull
+
+            ->assert('With valid id')
+                ->given($this->newTestedInstance($id))
+                ->then
+                    ->string($this->testedInstance->getId())
+                        ->isIdenticalTo($id)
+
+                    ->object($date = $this->testedInstance->getCreationDate())
+                        ->isInstanceOf('DateTime')
+                    ->variable($date->format('U'))
+                        ->isEqualTo($timestamp)
+
+            ->assert('With bad credential')
+                ->given($key = uniqid())
+                ->then
+                    ->exception(function () use ($key) {
+                        $this->newTestedInstance($key);
+                    })
+                        ->isInstanceOf(Exceptions\NotAuthorizedException::class)
+                        ->hasNestedException
+                        ->message
+                            ->isIdenticalTo('You are not authorized to access that resource')
+        ;
+
+        $errors = [
+            310 => [
+                'expected' => Exceptions\TooManyRedirectsException::class,
+                'thrown' => GuzzleHttp\Exception\TooManyRedirectsException::class,
+                'message' => 'Too many redirect',
+            ],
+            400 => [
+                'expected' => Exceptions\BadRequestException::class,
+                'thrown' => GuzzleHttp\Exception\ClientException::class,
+                'message' => 'Bad request',
+            ],
+            402 => [ // not handled
+                'expected' => Exceptions\ClientException::class,
+                'thrown' => GuzzleHttp\Exception\ClientException::class,
+                'message' => 'Payment required',
+            ],
+            403 => [ // not handled
+                'expected' => Exceptions\ClientException::class,
+                'thrown' => GuzzleHttp\Exception\ClientException::class,
+                'message' => 'Forbidden',
+            ],
+            404 => [
+                'expected' => Exceptions\NotFoundException::class,
+                'thrown' => GuzzleHttp\Exception\ClientException::class,
+                'message' => 'Not found',
+            ],
+            405 => [ // not handled
+                'expected' => Exceptions\ClientException::class,
+                'thrown' => GuzzleHttp\Exception\ClientException::class,
+                'message' => 'Method Not Allowed',
+            ],
+            406 => [ // not handled
+                'expected' => Exceptions\ClientException::class,
+                'thrown' => GuzzleHttp\Exception\ClientException::class,
+                'message' => 'Not Acceptable',
+            ],
+            407 => [ // not handled
+                'expected' => Exceptions\ClientException::class,
+                'thrown' => GuzzleHttp\Exception\ClientException::class,
+                'message' => 'Proxy Authentication Required',
+            ],
+            408 => [ // not handled
+                'expected' => Exceptions\ClientException::class,
+                'thrown' => GuzzleHttp\Exception\ClientException::class,
+                'message' => 'Request Time-out',
+            ],
+            409 => [ // not handled
+                'expected' => Exceptions\ClientException::class,
+                'thrown' => GuzzleHttp\Exception\ClientException::class,
+                'message' => 'Conflict',
+            ],
+            410 => [ // not handled
+                'expected' => Exceptions\ClientException::class,
+                'thrown' => GuzzleHttp\Exception\ClientException::class,
+                'message' => 'Gone',
+            ],
+            500 => [
+                'expected' => Exceptions\ServerException::class,
+                'thrown' => GuzzleHttp\Exception\ServerException::class,
+                'message' => 'Internal Server Error',
+            ],
+        ];
+
+        foreach ($errors as $code => $infos) {
+            $this
+                ->assert(sprintf('%d - %s', $code, $infos['message']))
+                    ->given($key = uniqid())
+                    ->and($request = new Request('GET', $api->getUri()))
+                    ->and($response = new Response($code))
+                    ->and($exception = $infos['thrown'])
+                    ->and($mock = new MockHandler([
+                        new $exception($infos['message'], $request, $response),
+                    ]))
+                    ->and($handler = HandlerStack::create($mock))
+                    ->and($client = new Client(['handler' => $handler]))
+                    ->and($api->setHttpClient($client))
+                    ->then
+                        ->exception(function () use ($key) {
+                            $this->newTestedInstance($key);
+                        })
+                            ->isInstanceOf($infos['expected'])
+                            ->hasNestedException
+            ;
+        }
+    }
+
     public function testGetEndpoint()
     {
         $this
