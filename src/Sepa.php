a470e4887c395f481449385e2842819502b5e377
Joel <jdasilva@corp.free.fr>
2018-10-08 15:12:16 +0200

    Add `Sepa::setIban()` method

--- src/Sepa.php
+++ src/Sepa.php
@@ -41,4 +41,46 @@ class Sepa extends Api\Object
 
         return $this;
     }
+
+    /**
+     * Add or update an International Bank Account Number (IBAN)
+     *
+     * @param string $iban An International Bank Account Number
+     * @return self
+     * @throws ild78\Exceptions\InvalidArgumentException when IBAN is invalid
+     */
+    public function setIban(string $iban) : self
+    {
+        $iban = str_replace(' ', '', $iban);
+        $country = substr($iban, 0, 2);
+        $start = substr($iban, 0, 4);
+        $bban = substr($iban, 4);
+        $code = strtoupper($bban . $start);
+
+        $letters = range('A', 'Z');
+        $replace = [];
+        $index = 10;
+
+        foreach ($letters as $letter) {
+            $replace[$letter] = $index++;
+        }
+
+        $code = str_replace(array_keys($replace), $replace, $code);
+
+        $check = substr($code, 0, 2);
+        $parts = explode(' ', chunk_split(substr($code, 2), 7, ' '));
+
+        foreach ($parts as $part) {
+            $check = ($check . $part) % 97;
+        }
+
+        if ($check !== 1) {
+            throw new Exceptions\InvalidArgumentException(sprintf('"%s" is not a valid IBAN', $iban));
+        }
+
+        $this->country = $country;
+        $this->iban = $iban;
+
+        return $this;
+    }
 }
--- tests/unit/Sepa.php
+++ tests/unit/Sepa.php
@@ -55,4 +55,104 @@ class Sepa extends atoum
             }
         }
     }
+
+    public function testSetIban()
+    {
+        $this
+            ->assert('Add a valid french random IBAN')
+                ->if($bban = rand())
+                ->and($country = 'FR')
+                ->and($validation = $bban . '1527' . '00') // 15 => F / 27 => R
+                ->and($check = sprintf('%02d', 98 - ($validation % 97)))
+                ->and($iban = $country . $check . $bban)
+
+                ->if($this->newTestedInstance)
+                ->then
+                    ->variable($this->testedInstance->getCountry())
+                        ->isNull
+
+                    ->variable($this->testedInstance->getIban())
+                        ->isNull
+
+                    ->object($this->testedInstance->setIban($iban))
+                        ->isTestedInstance
+
+                    ->string($this->testedInstance->getCountry())
+                        ->isIdenticalTo($country)
+
+                    ->string($this->testedInstance->getIban())
+                        ->isIdenticalTo($iban)
+
+            ->assert('Add a valid BE IBAN from wikipedia')
+                ->given($iban = 'BE71096123456769')
+                ->and($country = substr($iban, 0, 2))
+                ->and($this->newTestedInstance)
+                ->then
+                    ->variable($this->testedInstance->getCountry())
+                        ->isNull
+
+                    ->variable($this->testedInstance->getIban())
+                        ->isNull
+
+                    ->object($this->testedInstance->setIban($iban))
+                        ->isTestedInstance
+
+                    ->string($this->testedInstance->getCountry())
+                        ->isIdenticalTo($country)
+
+                    ->string($this->testedInstance->getIban())
+                        ->isIdenticalTo($iban)
+
+            ->assert('Add a valid GB IBAN from wikipedia')
+                ->given($iban = 'GB82WEST12345698765432')
+                ->and($country = substr($iban, 0, 2))
+                ->and($this->newTestedInstance)
+                ->then
+                    ->variable($this->testedInstance->getCountry())
+                        ->isNull
+
+                    ->variable($this->testedInstance->getIban())
+                        ->isNull
+
+                    ->object($this->testedInstance->setIban($iban))
+                        ->isTestedInstance
+
+                    ->string($this->testedInstance->getCountry())
+                        ->isIdenticalTo($country)
+
+                    ->string($this->testedInstance->getIban())
+                        ->isIdenticalTo($iban)
+
+            ->assert('It should accept space for readeability')
+                ->given($iban = 'GR96 0810 0010 0000 0123 4567 890')
+                ->and($country = substr($iban, 0, 2))
+                ->and($this->newTestedInstance)
+                ->then
+                    ->variable($this->testedInstance->getCountry())
+                        ->isNull
+
+                    ->variable($this->testedInstance->getIban())
+                        ->isNull
+
+                    ->object($this->testedInstance->setIban($iban))
+                        ->isTestedInstance
+
+                    ->string($this->testedInstance->getCountry())
+                        ->isIdenticalTo($country)
+
+                    ->string($this->testedInstance->getIban())
+                        ->isIdenticalTo(str_replace(' ', '', $iban))
+
+            ->assert('Add an invalid IBAN')
+                ->given($iban = 'FR87BARC20658244971655')
+                ->and($this->newTestedInstance)
+                ->then
+                    ->exception(function () use ($iban) {
+                        $this->testedInstance->setIban($iban);
+                    })
+                        ->isInstanceOf(Exceptions\InvalidArgumentException::class)
+                        ->message
+                            ->contains($iban)
+        ;
+    }
 }
