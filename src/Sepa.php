3d4e6f964dc01a5c864703f9db723d760bc85b72
Joel <jdasilva@corp.free.fr>
2018-10-08 12:55:40 +0200

    Add some BIC validation
    Basic validation

--- src/Sepa.php
+++ src/Sepa.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace ild78;
 
+use ild78\Exceptions;
+
 /**
  * Representation of a SEPA account
  */
@@ -19,4 +21,24 @@ class Sepa extends Api\Object
 
     /** @var string|null */
     protected $name;
+
+    /**
+     * Add or update a Bank Identifier Code (BIC)
+     *
+     * @param string $bic A Bank Identifier Code
+     * @return self
+     * @throws ild78\Exceptions\InvalidArgumentException when BIC seems invalid
+     */
+    public function setBic($bic) : self
+    {
+        $length = strlen($bic);
+
+        if ($length !== 8 && $length !== 11) {
+            throw new Exceptions\InvalidArgumentException(sprintf('"%s" is not a valid BIC', $bic));
+        }
+
+        $this->bic = $bic;
+
+        return $this;
+    }
 }
--- tests/unit/Sepa.php
+++ tests/unit/Sepa.php
@@ -4,6 +4,7 @@ namespace ild78\tests\unit;
 
 use atoum;
 use ild78\Api;
+use ild78\Exceptions;
 use ild78\Sepa as testedClass;
 
 class Sepa extends atoum
@@ -14,5 +15,44 @@ class Sepa extends atoum
             ->class(testedClass::class)
                 ->isSubclassOf(Api\Object::class)
         ;
+    }
+
+    public function testSetBic()
+    {
+        $range = range(1, 20);
+
+        foreach ($range as $index) {
+            $isValid = $index === 8 || $index === 11;
+            $message = sprintf('%d chars => %s', $index, $isValid ? 'valid' : 'invalid');
+
+            $this
+                ->assert($message)
+                    ->given($this->newTestedInstance)
+                    ->and($bic = substr(md5(uniqid()), 0, $index))
+                    ->then // see below
+            ;
+
+            if ($isValid) {
+                $this
+                    ->variable($this->testedInstance->getBic())
+                        ->isNull
+
+                    ->object($this->testedInstance->setBic($bic))
+                        ->isTestedInstance
+
+                    ->string($this->testedInstance->getBic())
+                        ->isIdenticalTo($bic)
+                ;
+            } else {
+                $this
+                    ->exception(function () use ($bic) {
+                        $this->testedInstance->setBic($bic);
+                    })
+                        ->isInstanceOf(Exceptions\InvalidArgumentException::class)
+                        ->message
+                            ->contains($bic)
+                ;
+            }
+        }
     }
 }
