a25177d034da010755fd6290e8e207c35929a1d2
Joel <jdasilva@corp.free.fr>
2018-10-24 15:01:12 +0200

    Add capacity to get request and response from it

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -3,9 +3,55 @@ declare(strict_types=1);
 
 namespace ild78\Exceptions;
 
+use GuzzleHttp\Exception\RequestException;
+use Throwable;
+use Psr;
+
 /**
  * Base exception class for all ild78 HTTP based exceptions.
  */
 class HttpException extends Exception
 {
+    /** @var Psr\Http\Message\RequestInterface|null */
+    protected $request;
+
+    /** @var Psr\Http\Message\ResponseInterface|null */
+    protected $response;
+
+    /**
+     * Construct the exception
+     *
+     * @param string $message The Exception message to throw.
+     * @param integer $code The Exception code.
+     * @param Throwable $previous The previous exception used for the exception chaining.
+     */
+    public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        if ($previous instanceof RequestException) {
+            $this->request = $previous->getRequest();
+            $this->response = $previous->getResponse();
+        }
+    }
+
+    /**
+     * Get the request that caused the exception
+     *
+     * @return Psr\Http\Message\RequestInterface|null
+     */
+    public function getRequest() : ?Psr\Http\Message\RequestInterface
+    {
+        return $this->request;
+    }
+
+    /**
+     * Get the associated response
+     *
+     * @return Psr\Http\Message\ResponseInterface|null
+     */
+    public function getResponse() : ?Psr\Http\Message\ResponseInterface
+    {
+        return $this->response;
+    }
 }
--- tests/unit/Exceptions/HttpException.php
+++ tests/unit/Exceptions/HttpException.php
@@ -16,4 +16,47 @@ class HttpException extends atoum
                 ->isSubclassOf(Exceptions\Exception::class)
         ;
     }
+
+    public function testGetRequest()
+    {
+        $this
+            ->assert('Return null when nothing particular is done')
+                ->given($this->newTestedInstance())
+                ->then
+                    ->variable($this->testedInstance->getRequest())
+                        ->isNull
+
+            ->assert('Return object when a Guzzle RequestException is used')
+                ->given($request = new mock\Psr\Http\Message\RequestInterface)
+
+                ->and($previous = new GuzzleHttp\Exception\RequestException(uniqid(), $request))
+
+                ->if($this->newTestedInstance(uniqid(), rand(0, 100), $previous))
+                ->then
+                    ->object($this->testedInstance->getRequest())
+                        ->isInstanceOf($request)
+        ;
+    }
+
+    public function testGetResponse()
+    {
+        $this
+            ->assert('Return null when nothing particular is done')
+                ->given($this->newTestedInstance())
+                ->then
+                    ->variable($this->testedInstance->getResponse())
+                        ->isNull
+
+            ->assert('Return object when a Guzzle RequestException is used')
+                ->given($request = new mock\Psr\Http\Message\RequestInterface)
+                ->and($response = new mock\Psr\Http\Message\ResponseInterface)
+
+                ->and($previous = new GuzzleHttp\Exception\RequestException(uniqid(), $request, $response))
+
+                ->if($this->newTestedInstance(uniqid(), rand(0, 100), $previous))
+                ->then
+                    ->object($this->testedInstance->getResponse())
+                        ->isInstanceOf($response)
+        ;
+    }
 }
