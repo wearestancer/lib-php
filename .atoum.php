9fa3a328b1020eec4234bc1a8a3ebd842bfae28f
Joel <jdasilva@corp.free.fr>
2018-10-25 12:47:26 +0200

    Creating code coverage only when possible

--- .atoum.php
+++ .atoum.php
@@ -15,12 +15,14 @@ $runner
 $runner->addExtension(new mageekguy\atoum\jsonSchema\extension($script));
 
 // Reports (for HTML coverage)
-$script->addDefaultReport();
+if (extension_loaded('xdebug') === true) {
+    $script->addDefaultReport();
 
-$coverage = new mageekguy\atoum\reports\coverage\html();
-$coverage
-    ->addWriter(new mageekguy\atoum\writers\std\out())
-    ->setOutPutDirectory(__DIR__ . '/reports/coverage')
-;
+    $coverage = new mageekguy\atoum\reports\coverage\html();
+    $coverage
+        ->addWriter(new mageekguy\atoum\writers\std\out())
+        ->setOutPutDirectory(__DIR__ . '/reports/coverage')
+    ;
 
-$runner->addReport($coverage);
+    $runner->addReport($coverage);
+}
