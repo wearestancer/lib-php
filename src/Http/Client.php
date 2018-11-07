5f199d79ae681a0ce6214489dcef31c39258476a
Joel <jdasilva@corp.free.fr>
2018-11-07 15:09:18 +0100

    Prepare cURL instance

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -8,4 +8,27 @@ namespace ild78\Http;
  */
 class Client
 {
+    /** @var ressource */
+    protected $curl;
+
+    /**
+     * Creation of a new client instance
+     *
+     * This only start a new cURL ressource
+     */
+    public function __construct()
+    {
+        $this->curl = curl_init();
+    }
+
+    /**
+     * Close cURL ressource on destruction
+     */
+    public function __destruct()
+    {
+        if ($this->curl) {
+            curl_close($this->curl);
+            $this->curl = null;
+        }
+    }
 }
--- tests/unit/Http/Client.php
+++ tests/unit/Http/Client.php
@@ -7,4 +7,21 @@ use ild78;
 
 class Client extends atoum
 {
+    public function test__construct__destruct()
+    {
+        $this
+            ->given($ressource = uniqid())
+            ->and($this->function->curl_init = $ressource)
+            ->and($this->function->curl_close = true)
+            ->then
+                ->object($this->newTestedInstance)
+                ->function('curl_init')->wasCalled->once
+                ->function('curl_close')->wasCalled->never
+
+                ->variable($this->testedInstance->__destruct())
+                ->function('curl_close')
+                    ->wasCalledWithArguments($ressource)
+                        ->once
+        ;
+    }
 }
