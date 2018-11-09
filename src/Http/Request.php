ee44b7aecccbc6b68fdb112fdafea741377141b1
Joel <jdasilva@corp.free.fr>
2018-11-09 11:49:13 +0100

    Implements `Request::getMethod()`

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -14,4 +14,46 @@ use ild78;
 class Request
 {
     use MessageTrait;
+
+    /** @var string */
+    protected $method;
+
+    /** @var string */
+    protected $uri;
+
+    /**
+     * Create a response instance
+     *
+     * @param string $method HTTP method.
+     * @param string $uri URI.
+     * @param array $headers Request headers.
+     * @param string|null $body Request body.
+     * @param string $version Protocol version.
+     */
+    public function __construct(
+        string $method,
+        string $uri,
+        array $headers = [],
+        $body = null,
+        $version = '1.1'
+    ) {
+        $this->method = strtoupper($method);
+        $this->uri = $uri;
+        $this->body = $body;
+        $this->protocol = $version;
+
+        foreach ($headers as $name => $value) {
+            $this->headers[$name] = (array) $value;
+        }
+    }
+
+    /**
+     * Retrieves the HTTP method of the request.
+     *
+     * @return string Returns the request method.
+     */
+    public function getMethod() : string
+    {
+        return $this->method;
+    }
 }
--- tests/unit/Http/Request.php
+++ tests/unit/Http/Request.php
@@ -7,4 +7,15 @@ use Psr;
 
 class Request extends atoum
 {
+    public function testGetMethod()
+    {
+        $this
+            ->given($method = uniqid())
+            ->and($uri = uniqid())
+            ->if($this->newTestedInstance($method, $uri))
+            ->then
+                ->string($this->testedInstance->getMethod())
+                    ->isIdenticalTo(strtoupper($method))
+        ;
+    }
 }
