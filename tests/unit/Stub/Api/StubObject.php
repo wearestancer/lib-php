f96b5993125119f79757576bc1ff71dbc043df22
Joel <jdasilva@corp.free.fr>
2019-05-13 18:18:38 +0200

    Test that populated is transmitted to inner object

--- tests/unit/Stub/Api/StubObject.php
+++ tests/unit/Stub/Api/StubObject.php
@@ -936,6 +936,34 @@ class StubObject extends atoum
                             ->withArguments('GET')
                                 ->never
 
+            ->assert('Inner object are marked as populated too')
+                ->given($config = ild78\Api\Config::init(uniqid()))
+
+                ->if($client = new mock\GuzzleHttp\Client)
+                ->and($id = uniqid())
+                ->and($timestamp = time())
+                ->and($body = '{"id":"' . $id . '","created":' . $timestamp . ',"object2":"' . uniqid() . '","array4":["' . uniqid() . '"]}')
+                ->and($response = new GuzzleHttp\Psr7\Response(200, [], $body))
+                ->and($this->calling($client)->request = $response)
+                ->and($config->setHttpClient($client))
+
+                ->and($this->newTestedInstance($id))
+                ->then
+                    ->object($this->testedInstance->populate())
+
+                    ->mock($client)
+                        ->call('request')
+                            ->once
+
+                    ->boolean($this->testedInstance->getObject2()->testOnlyGetPopulated())
+                        ->isTrue
+
+                    ->array($array4 = $this->testedInstance->getArray4())
+                        ->hasSize(1)
+
+                    ->boolean($array4[0]->testOnlyGetPopulated())
+                        ->isTrue
+
             ->assert('Populate working normally')
                 ->given($config = ild78\Api\Config::init(uniqid()))
                 ->and($id = uniqid())
