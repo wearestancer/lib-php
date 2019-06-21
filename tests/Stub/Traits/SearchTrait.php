20c381179f6643b30ce3061eebdf276692e5638d
Joel <jdasilva@corp.free.fr>
2019-06-21 18:19:59 +0200

    Add tests for `SearchTrait`

---
+++ tests/Stub/Traits/SearchTrait.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ild78\Stub\Traits;
+
+use ild78;
+
+class SearchTrait extends ild78\Stub\Api\StubObject
+{
+    use ild78\Traits\SearchTrait;
+
+    protected $allowedSearchFilter = [];
+}
---
+++ tests/unit/Stub/Traits/SearchTrait.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace ild78\tests\unit\Stub\Traits;
+
+use DateInterval;
+use DateTime;
+use ild78;
+use ild78\Stub\Traits\SearchTrait as testedClass;
+use mock;
+
+class SearchTrait extends ild78\Tests\atoum
+{
+    public function testSearch()
+    {
+        $this
+            ->given($client = new mock\ild78\Http\Client)
+            ->and($response = new mock\ild78\Http\Response(200))
+            ->and($body = file_get_contents(__DIR__ . '/../../fixtures/payment/list.json'))
+            ->and($this->calling($response)->getBody = $body)
+            ->and($this->calling($client)->request = $response)
+            ->and($config = ild78\Api\Config::init(['stest_' . bin2hex(random_bytes(12))]))
+            ->and($config->setHttpClient($client))
+
+            ->and($options = [
+                'headers' => [
+                    'Authorization' => $config->getBasicAuthHeader(),
+                    'Content-Type' => 'application/json',
+                    'User-Agent' => $config->getDefaultUserAgent(),
+                ],
+                'timeout' => $config->getTimeout(),
+            ])
+
+            ->and($this->newTestedInstance)
+
+            ->assert('Invalid limit')
+                ->exception(function () {
+                    $this->invoke($this->testedInstance)->search(['limit' => 0]);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchLimit::class)
+                    ->message
+                        ->isIdenticalTo('Limit must be between 1 and 100.')
+
+                ->exception(function () {
+                    $this->invoke($this->testedInstance)->search(['limit' => 101]);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchLimit::class)
+                    ->message
+                        ->isIdenticalTo('Limit must be between 1 and 100.')
+
+                ->exception(function () {
+                    $this->invoke($this->testedInstance)->search(['limit' => uniqid()]);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchLimit::class)
+                    ->message
+                        ->isIdenticalTo('Limit must be between 1 and 100.')
+
+            ->assert('Invalid start')
+                ->exception(function () {
+                    $this->invoke($this->testedInstance)->search(['start' => -1]);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchStart::class)
+                    ->message
+                        ->isIdenticalTo('Start must be a positive integer.')
+
+                ->exception(function () {
+                    $this->invoke($this->testedInstance)->search(['start' => uniqid()]);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchStart::class)
+                    ->message
+                        ->isIdenticalTo('Start must be a positive integer.')
+
+            ->assert('No terms')
+                ->exception(function () {
+                    $this->invoke($this->testedInstance)->search([]);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchFilter::class)
+                    ->message
+                        ->isIdenticalTo('Invalid search filters.')
+
+                ->exception(function () {
+                    $this->invoke($this->testedInstance)->search(['foo' => 'bar']);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchFilter::class)
+                    ->message
+                        ->isIdenticalTo('Invalid search filters.')
+
+            ->assert('Invalid created filter')
+                ->exception(function () {
+                    $this->invoke($this->testedInstance)->search(['created' => time() + 100]);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchCreationFilter::class)
+                    ->message
+                        ->isIdenticalTo('Created must be in the past.')
+
+                ->exception(function () {
+                    $date = new DateTime();
+                    $date->add(new DateInterval('P1D'));
+
+                    $this->invoke($this->testedInstance)->search(['created' => $date]);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchCreationFilter::class)
+                    ->message
+                        ->isIdenticalTo('Created must be in the past.')
+
+                ->exception(function () {
+                    $this->invoke($this->testedInstance)->search(['created' => 0]);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchCreationFilter::class)
+                    ->message
+                        ->isIdenticalTo('Created must be a position integer or a DateTime object.')
+
+                ->exception(function () {
+                    $this->invoke($this->testedInstance)->search(['created' => uniqid()]);
+                })
+                    ->isInstanceOf(ild78\Exceptions\InvalidSearchCreationFilter::class)
+                    ->message
+                        ->isIdenticalTo('Created must be a position integer or a DateTime object.')
+
+            ->assert('Make request')
+                ->if($limit = rand(1, 100))
+                ->and($start = rand(0, PHP_INT_MAX))
+                ->and($orderId = uniqid())
+                ->and($created = time() - rand(10, 1000000))
+
+                ->and($location = $this->testedInstance->getUri())
+                ->and($terms1 = [
+                    'created' => $created,
+                    'limit' => $limit,
+                    'start' => $start,
+                ])
+                ->and($location1 = $location . '?' . http_build_query($terms1))
+
+                ->and($terms2 = [
+                    'created' => $created,
+                    'limit' => $limit,
+                    'start' => $start + 2, // Forced in json sample
+                ])
+                ->and($location2 = $location . '?' . http_build_query($terms2))
+                ->then
+                    ->generator($gen = $this->invoke($this->testedInstance)->search($terms1))
+                        ->yields
+                            ->object
+                                ->isInstanceOf(testedClass::class)
+                                ->toString
+                                    ->isIdenticalTo('"paym_JnU7xyTGJvxRWZuxvj78qz7e"') // From json sample
+
+                    ->mock($client)
+                        ->call('request')
+                            ->withArguments('GET', $location1, $options)
+                                ->once
+                            ->withArguments('GET', $location2, $options)
+                                ->never
+
+                    ->generator($gen)
+                        ->yields
+                            ->object
+                                ->isInstanceOf(testedClass::class)
+                                ->toString
+                                    ->isIdenticalTo('"paym_p5tjCrXHy93xtVtVqvEJoC1c"') // From json sample
+                        ->yields
+                            ->object
+                                ->isInstanceOf(testedClass::class)
+                                ->toString
+                                    ->isIdenticalTo('"paym_JnU7xyTGJvxRWZuxvj78qz7e"') // From json sample
+
+                    ->mock($client)
+                        ->call('request')
+                            ->withArguments('GET', $location1, $options)
+                                ->once // Called the first time
+                            ->withArguments('GET', $location2, $options)
+                                ->once
+
+            ->assert('Empty response')
+                ->given($body = [
+                    'payments' => [],
+                    'range' => [
+                        'has_more' => false,
+                        'limit' => 10,
+                    ],
+                ])
+                ->and($this->calling($response)->getBody = json_encode($body))
+
+                ->if($limit = rand(1, 100))
+                ->and($terms = [
+                    'limit' => $limit,
+                ])
+                ->and($query = http_build_query(['limit' => $limit, 'start' => 0]))
+                ->and($location = $this->testedInstance->getUri() . '?' . $query)
+                ->then
+                    ->generator($gen = $this->invoke($this->testedInstance)->search($terms))
+                        ->yields
+                            ->variable
+                                ->isNull
+
+                    ->mock($client)
+                        ->call('request')
+                            ->withArguments('GET', $location, $options)
+                                ->once
+
+            ->assert('Invalid response')
+                ->given($this->calling($response)->getBody = null)
+
+                ->if($limit = rand(1, 100))
+                ->and($terms = [
+                    'limit' => $limit,
+                ])
+                ->and($query = http_build_query(['limit' => $limit, 'start' => 0]))
+                ->and($location = $this->newTestedInstance->getUri() . '?' . $query)
+                ->then
+                    ->generator($gen = $this->invoke($this->testedInstance)->search($terms))
+                        ->yields
+                            ->variable
+                                ->isNull
+
+                    ->mock($client)
+                        ->call('request')
+                            ->withArguments('GET', $location, $options)
+                                ->once
+        ;
+    }
+}
