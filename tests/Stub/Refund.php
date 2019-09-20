9137b8890cb750f573d81d5a7211e047631ddfb5
Joel <jdasilva@corp.free.fr>
2019-09-20 19:13:17 +0200

    Add test on `Refund::save()`

--- tests/Stub/Refund.php
+++ tests/Stub/Refund.php
@@ -7,4 +7,11 @@ use ild78;
 class Refund extends ild78\Refund
 {
     use ild78\Stub\TestMethodTrait;
+
+    public function testOnlySetPayment(ild78\Payment $payment) : self
+    {
+        $this->payment = $payment;
+
+        return $this;
+    }
 }
