ec986c9e96411d9128f61d479d357cbc34cfef97
Joel <jdasilva@corp.free.fr>
2019-11-16 21:28:24 +0100

    Fix `InvalidSearchStartException`

--- src/Exceptions/InvalidSearchStartException.php
+++ src/Exceptions/InvalidSearchStartException.php
@@ -4,16 +4,12 @@ declare(strict_types=1);
 namespace ild78\Exceptions;
 
 use ild78\Interfaces\ExceptionInterface;
-use Psr;
 
 /**
  * Exception thrown on invalid search start.
  */
 class InvalidSearchStartException extends InvalidArgumentException implements ExceptionInterface
 {
-    /** @var string Default log level */
-    protected static $logLevel = Psr\Log\LogLevel::DEBUG;
-
     /**
      * Return default message for that kind of exception
      *
