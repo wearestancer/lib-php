805c05434c7953095e339bf5f38adaf7c0676b0c
Joel <jdasilva@corp.free.fr>
2019-11-16 22:07:13 +0100

    Fix `MissingPaymentIdException`

--- src/Exceptions/MissingPaymentIdException.php
+++ src/Exceptions/MissingPaymentIdException.php
@@ -9,10 +9,10 @@ use Psr;
 /**
  * Exception thrown when no payment ID was setted and an operation needs it.
  */
-class MissingPaymentIdException extends InvalidArgumentException implements ExceptionInterface
+class MissingPaymentIdException extends BadMethodCallException implements ExceptionInterface
 {
     /** @var string Default log level */
-    protected static $logLevel = Psr\Log\LogLevel::ERROR;
+    protected static $logLevel = Psr\Log\LogLevel::CRITICAL;
 
     /**
      * Return default message for that kind of exception
