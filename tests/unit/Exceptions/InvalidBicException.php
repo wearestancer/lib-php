68794b1490fb77895dae261a5e3ab43430256a6f
Joel <jdasilva@corp.free.fr>
2019-11-16 21:09:03 +0100

    Fix `InvalidBicException`

--- tests/unit/Exceptions/InvalidBicException.php
+++ tests/unit/Exceptions/InvalidBicException.php
@@ -22,7 +22,7 @@ class InvalidBicException extends ild78\Tests\atoum
             ->if($class = $this->testedClass->getClass())
             ->then
                 ->string($class::getDefaultMessage())
-                    ->isIdenticalTo('Invalid BIC')
+                    ->isIdenticalTo('Invalid BIC.')
         ;
     }
 
