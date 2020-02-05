6a451e874059795b467c3de5c23a46e3007aa130
Joel <jdasilva@corp.free.fr>
2020-02-05 17:24:27 +0100

    Add new exception on external ids

---
+++ tests/unit/Exceptions/InvalidExternalIdException.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ild78\tests\unit\Exceptions;
+
+use ild78;
+use Psr;
+
+class InvalidExternalIdException extends ild78\Tests\atoum
+{
+    public function testClass()
+    {
+        $this
+            ->currentlyTestedClass
+                ->extends(ild78\Exceptions\InvalidArgumentException::class)
+                ->implements(ild78\Interfaces\ExceptionInterface::class)
+        ;
+    }
+
+    public function testGetDefaultMessage()
+    {
+        $this
+            ->if($class = $this->testedClass->getClass())
+            ->then
+                ->string($class::getDefaultMessage())
+                    ->isIdenticalTo('Invalid external ID.')
+        ;
+    }
+
+    public function testGetLogLevel()
+    {
+        $this
+            ->if($class = $this->testedClass->getClass())
+            ->then
+                ->string($class::getLogLevel())
+                    ->isIdenticalTo(Psr\Log\logLevel::DEBUG)
+        ;
+    }
+}
