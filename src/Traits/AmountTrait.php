ad80e330fb1530aad9187d359391c2a896967435
Joel <jdasilva@corp.free.fr>
2020-02-24 19:15:25 +0100

    Prevent losing 1 when converting to integer
    See:
    - https://www.php.net/manual/fr/function.intval.php#101439
    - https://www.php.net/manual/fr/function.intval.php#60793

--- src/Traits/AmountTrait.php
+++ src/Traits/AmountTrait.php
@@ -12,14 +12,17 @@ trait AmountTrait
     /**
      * Update amount
      *
-     * @param integer $amount New amount.
+     * We allow float as input to prevent error during converting to integer.
+     * We will not use the floating part.
+     *
+     * @param float $amount New amount.
      * @return self
      * @throws ild78\Exceptions\InvalidAmountException When the amount is invalid.
      */
-    public function setAmount(int $amount): self
+    public function setAmount(float $amount): self
     {
         try {
-            return parent::setAmount($amount);
+            return parent::setAmount(intval(strval($amount)));
         } catch (ild78\Exceptions\InvalidArgumentException $excep) {
             throw new ild78\Exceptions\InvalidAmountException($excep->getMessage(), $excep->getCode(), $excep);
         }
