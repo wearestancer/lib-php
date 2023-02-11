15404b98324f0d8df4ff26bdc0219c8e03f991af
Joel <jdasilva@stancer.com>
2023-02-11 14:14:54 +0100

    Remove `ramsey/uuid`

--- tests/atoum.php
+++ tests/atoum.php
@@ -5,7 +5,6 @@ namespace Stancer\Tests;
 use atoum\atoum as base;
 use Stancer;
 use Faker;
-use Ramsey\Uuid\Uuid;
 
 class atoum extends base\test
 {
@@ -88,6 +87,11 @@ class atoum extends base\test
 
     public function getUuid(): string
     {
-        return Uuid::uuid4()->toString();
+        $data = random_bytes(16);
+
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 }
